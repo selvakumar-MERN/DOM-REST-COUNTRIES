6294df51b70f4c2d2ac475efa02d5ea0d9b9116b
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-05-04 14:08:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ then(res => res.json())
 .then((val) =>{ 
 for (i of val){
 
-//console.log(i.latlng)
-
 const col1=document.createElement("div")
 row.append(col1)
 col1.setAttribute("class","col-sm-6 col-md-4 col-lg-4 col-xl-4 column1")
@@ -36,7 +34,6 @@ const cardbody1=document.createElement("div")
 card1.append(cardbody1)
 card1.setAttribute("lat",i.latlng[0])
 card1.setAttribute("lon",i.latlng[1])
-//console.log(i.latlng[0])
 cardbody1.setAttribute("class","card-body cb")
 const img= document.createElement("img")
 cardbody1.append(img)
@@ -62,15 +59,12 @@ button.addEventListener("click",async()=>{
 const cname=cardhead1.getAttribute("cname")
  const cardlat=card1.getAttribute("lat")
  const cardlon=card1.getAttribute("lon")   
- console.log(cardlat)
  try{
       await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cardlat}&lon=${cardlon}&appid=24d7df24b594d5c510df675d6f9bef3a`)
 .then((res)=>res.json())   
 .then((data)=>{
-    console.log(data)
     const weather=data.weather
     for(j of weather)
-    //console.log(j)
 
 cardtext1.innerHTML+=`<span>Weather</span><br><p><label>Main : </label> ${j.main}<br><label>Description : </label> ${j.description}<br>
 <label>Wind Speed : </label> ${data.wind.speed}<br></p>`  
